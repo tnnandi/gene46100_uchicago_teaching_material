2f25aea2558e84b5776c697867729d311c6eda64
Christina Theodoris <ctheodoris@arrietty-gpu01.gladstone.internal>
2023-11-20 22:20:35 -0800

    Add functions for extracting gene embeddings, move state_embs_dict outside isp, fix bugs in isp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="geneformer",
-    version="0.0.1",
+    version="0.1.0",
     author="Christina Theodoris",
     author_email="christina.theodoris@gladstone.ucsf.edu",
     description="Geneformer is a transformer model pretrained \
