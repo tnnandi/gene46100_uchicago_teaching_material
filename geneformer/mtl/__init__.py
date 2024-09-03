7eca2695af42019bcf354e250c1f2bb222437d21
Christina Theodoris <ctheodoris@users.noreply.huggingface.co>
2024-09-03 17:40:13 +0000

    remove token dictionary and unpickling from init (#403)
    - nested import for dictionaries from parent (3b4533faff40ad00e85ec014cae258b31cdd2647)
    
    
    Co-authored-by: Madhavan Venkatesh <madhavanvenkatesh@users.noreply.huggingface.co>

--- geneformer/mtl/__init__.py
+++ geneformer/mtl/__init__.py
@@ -1,11 +1,1 @@
 # ruff: noqa: F401
-from .. import TOKEN_DICTIONARY_FILE
-import pickle
-
-def load_token_dictionary():
-    with open(TOKEN_DICTIONARY_FILE, 'rb') as f:
-        return pickle.load(f)
-
-TOKEN_DICTIONARY = load_token_dictionary()
-
-__all__ = ["TOKEN_DICTIONARY"]
